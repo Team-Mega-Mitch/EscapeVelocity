07af1a9ccf5b32920b7ad4a921eec2af4902c6b8
SebMorales1 <sebmorales1@gmail.com>
2022-01-22 15:16:08 -0500

    test commit

---
+++ Assets/UI/SCRIPTS/BOOST_MANAGER.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BOOSTMANAGER : MonoBehaviour
+{
+    public TMP_Text textBoost;
+    public float boost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
