eb9093f7ea46fad09a960619bc666e34739026c7
Mitch Youker <mitch.youker@gmail.com>
2022-01-22 14:22:34 -0500

    Fix breaking error with PlayerController

---
+++ Assets/Player/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D theRB;
+
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+
+        Vector3 mouse = Input.mousePosition;
+        Debug.Log(Input.mousePosition);
+    }
+}
