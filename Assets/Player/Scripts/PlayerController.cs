4cd8215be826206332bc10ba8e85a7527d803b82
Mitch Youker <mitch.youker@gmail.com>
2022-01-22 15:03:55 -0500

    Fix for movement and hide cursor

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -6,27 +6,27 @@ public class PlayerController : MonoBehaviour {
 
     public Rigidbody2D theRB;
 
-    public float moveSpeed;
+    public float movementSpeed;
 
-    private Camera theCam;
+    private Camera camera;
 
     // Start is called before the first frame update
     void Start() {
-        theCam = Camera.main;
+        camera = Camera.main;
+
+        //Set Cursor to not be visible
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update() {
-        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
-
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * movementSpeed;
         Vector3 mouse = Input.mousePosition;
-        // Debug.Log(Input.mousePosition);
 
-        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
-
+        Vector3 screenPoint = camera.WorldToScreenPoint(transform.localPosition);
         Vector2 offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
 
         float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        transform.rotation = Quaternion.Euler(0f, 0f, angle - 90);
     }
 }
