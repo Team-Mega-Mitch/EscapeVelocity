900f4671c710ebf8a0dabe5fa82182c1fe617304
Savannah Perkins <sbanana70@gmail.com>
2022-01-22 13:45:26 -0500

    Testing again

--- Assets/Savannah.cs
+++ Assets/Savannah.cs
@@ -7,8 +7,8 @@ public class Savannah : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      Debug.Log("test");
-        
+      Debug.Log("testing");
+
     }
 
     // Update is called once per frame
