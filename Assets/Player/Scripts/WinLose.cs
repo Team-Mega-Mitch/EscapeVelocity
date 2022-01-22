dd256360fcf3983f963b9e57766c97d337e64d64
Savannah Perkins <sbanana70@gmail.com>
2022-01-22 18:03:53 -0500

    Working Game Over Scene

--- Assets/Player/Scripts/WinLose.cs
+++ Assets/Player/Scripts/WinLose.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinLose : MonoBehaviour
 {
     
-    public GameObject LosePanel;
+    
     private bool gameOver;
     public void LoseLevel() 
     {
         if(!gameOver)
         {
         Debug.Log("You lose!");
-        LosePanel.SetActive(true);
+        SceneManager.LoadScene("LoseScreen");
         gameOver = true;
         }
     }
