9d8600ea421346ead4dc9008bcbf1724f744ea19
Nate-Perkins <52758095+Nate-Perkins@users.noreply.github.com>
2022-01-23 16:39:20 -0500

    Music Player and Audio Files

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -16,6 +16,7 @@ public class PlayerController : MonoBehaviour {
     private GameObject _Planet;
 
 
+
     public float thrust_multiplier = .01f;
 
     // Start is called before the first frame update
@@ -28,6 +29,7 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+
         mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0)) {
             if (thrust != maxThrust) {
@@ -39,6 +41,8 @@ public class PlayerController : MonoBehaviour {
     }
 
     void FixedUpdate() {
+        GameObject.FindGameObjectWithTag("Music").GetComponent<AudioController>().PlayMusic();
+
         float velocity = (minThrust + thrust + GravityBoost());
         Vector3 distance = new Vector3(0, 1, 0) * (minThrust + thrust + GravityBoost()) * Time.fixedDeltaTime;
         
