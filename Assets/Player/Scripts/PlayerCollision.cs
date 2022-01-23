f69113fdf2263dbc1a6c5d752342207f0a01a1c1
Mitch Youker <mitch.youker@gmail.com>
2022-01-23 02:31:52 -0500

    Hit checking

--- Assets/Player/Scripts/PlayerCollision.cs
+++ Assets/Player/Scripts/PlayerCollision.cs
@@ -1,17 +1,35 @@
 
 using UnityEngine;
 
-public class PlayerCollision : MonoBehaviour
-{
+public class PlayerCollision : MonoBehaviour {
     private WinLose _GameOver;
+    private bool collided = false;
+    private GameObject obj;
 
     private void Start() {
         _GameOver = GetComponent<WinLose>();
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.gameObject.name.Contains("Planet")) {
+        if (collision.gameObject.name.Contains("Planet")) {
             _GameOver.LoseLevel();
+        } else {
+            collided = true;
+            obj = collision.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+        collided = false;
+        obj = null;
+    }
+
+    void FixedUpdate() {
+        if (collided) {
+            Debug.Log("We hiting");
+            
+        } else {
+            Debug.Log("Where to hit???");
         }
     }
 }
